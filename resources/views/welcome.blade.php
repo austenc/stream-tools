afaa6e94ea6eed143e1e75da393cc9c21f4a8fae
Austen Cameron <austen.cameron@gmail.com>
2020-07-20 18:50:07 -0600

    initial commit - added tailwind css and alpine js

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Stream Tools</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div class="text-primary-500">
+            Welcome to stream tools
+        </div>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+let defaultConfig = require('tailwindcss/defaultConfig')
+
+module.exports = {
+    purge: [
+        './resources/**/*.php',
+        './resources/**/*.css',
+        './resources/**/*.js',
+    ],
+    theme: {
+        container: {
+            center: true,
+        },
+        colors: {
+            ...defaultConfig.theme.colors,
+            primary: defaultConfig.theme.colors.teal,
+        },
+    },
+}
