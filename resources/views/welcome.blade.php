24d4f5b989edbab884745b8dcfade59c93d4c3ab
Austen Cameron <austen.cameron@gmail.com>
2020-07-21 08:38:53 -0600

    added basic timer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,35 @@
         <title>Stream Tools</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
-    <body>
-        <div class="text-primary-500">
-            Welcome to stream tools
+    <body class="bg-gray-800">
+        <div x-data="timer()" x-init="start()">
+            <div class="text-white text-6xl font-londrina" x-text="timeRemaining()"></div>
         </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+        <script>
+            function timer() {
+                return {
+                    duration: 5,
+                    countdown: null,
+                    start() {
+                        this.duration = this.duration * 60 * 1000
+
+                        this.countdown = setInterval(() => {
+                            if (this.duration <= 0) {
+                                clearInterval(this.countdown)
+                                return
+                            }
+
+                            this.duration -= 1000
+                        }, 1000)
+                    },
+                    timeRemaining() {
+                        var minutes = Math.floor(this.duration / 60000);
+                        var seconds = ((this.duration % 60000) / 1000).toFixed(0);
+                        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+                    }
+                }
+            }
+        </script>
     </body>
 </html>
